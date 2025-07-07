09f0bb802225a6b1ab80594d3a16a76b2e1b87f0
Andrea Mancuso <andreamancuso@users.noreply.github.com>
2025-07-07 19:00:22 +0200

    Basic secret support (#19)

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,21 +1,24 @@
 type literal =
-  | Int of int
-  | String of string
-  | Bool of bool
-  | List of literal list
+  | Int       of int
+  | String    of string
+  | Bool      of bool
+  | List      of literal list
+  | Object    of (string * literal) list   (* nested maps, e.g. env = { … } *)
+  | SecretRef of string                    (* DATABASE_URL = secret "db-prod-url" *)
 
-type field =
-  | Field of string * literal
+type field = Field of string * literal
 
-type pod_decl = string * field list
-type svc_decl = string * field list
-type volume_decl = string * field list
-type enum_decl = string * string list
+type pod_decl     = string * field list          (* pod "nginx" { … }        *)
+type svc_decl     = string * field list          (* service "foo" { … }      *)
+type volume_decl  = string * field list          (* volume "bar" { … }       *)
+type enum_decl    = string * string list         (* enum "env" { … }         *)
+type secret_decl  = string * literal             (* secret "db-prod-url" { value = "…" } *)
 
 type decl =
-  | Pod of pod_decl
+  | Pod     of pod_decl
   | Service of svc_decl
-  | Volume of volume_decl
-  | Enum of enum_decl
+  | Volume  of volume_decl
+  | Enum    of enum_decl
+  | Secret  of secret_decl
 
 type program = decl list
